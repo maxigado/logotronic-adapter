cff40e94240dd58ba98f74259ef0f8e82176140c
fatihmehmetdag <fatihmehmetdag@outlook.com>
2025-11-27 10:09:49 +0300

    config update

--- logotronic-adapter-backend/src/config/config.ts
+++ logotronic-adapter-backend/src/config/config.ts
@@ -12,11 +12,34 @@ export const config = {
       write: "ie/d/j/simatic/v1/s7c1/dp/w/plc",
       metadata: "ie/m/j/simatic/v1/s7c1/dp",
       update: "ie/c/j/simatic/v1/updaterequest",
-      status: "ie/s/j/simatic/v1/s7c1/status",
+      status: "ie/d/j/simatic/v1/s7c1/status",
     },
   },
   logotronicserver: {
-    host: "localhost",
+    host: "192.168.0.166",
     port: 64001,
   },
 };
+
+// export const config = {
+//   application: {
+//     port: 3000,
+//   },
+//   databus: {
+//     url: "mqtt://localhost",
+//     username: "edge",
+//     password: "edge",
+//     client: "LogotronicAdapter",
+//     topic: {
+//       read: "ie/d/j/simatic/v1/s7c1/dp/r/plc/default",
+//       write: "ie/d/j/simatic/v1/s7c1/dp/w/plc",
+//       metadata: "ie/m/j/simatic/v1/s7c1/dp",
+//       update: "ie/c/j/simatic/v1/updaterequest",
+//       status: "ie/s/j/simatic/v1/s7c1/status",
+//     },
+//   },
+//   logotronicserver: {
+//     host: "localhost",
+//     port: 64001,
+//   },
+// };
