7a3a73242770c16aa59ca34a88c443897cfe8824
fatihmehmetdag <fatihmehmetdag@outlook.com>
2025-12-09 11:52:25 +0300

    fix

--- logotronic-adapter-backend/src/controller/api/route.ts
+++ logotronic-adapter-backend/src/controller/api/route.ts
@@ -11,7 +11,7 @@ router.get("/", (req: Request, res: Response) => {
   });
 });
 
-router.get("/status", getMachineStatus);
+router.get("/statusx", getMachineStatus);
 router.get("/tagstore", getTagStoreByID);
 router.get("/tagstore/:id", getTagDataById);
 router.get("/version", getVersion);
